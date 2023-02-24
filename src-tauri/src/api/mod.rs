ebf71fcd2ab68dd8fc53911c76d0266e6fb8e399
Othi <mnpq.raven@gmail.com>
2023-02-24 13:32:55 +0700

    add crud base

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -1,6 +1,9 @@
-use crate::prisma;
+mod crud;
+use crate::{algorithm::types::AlgoPiece, prisma, unit::types::Unit};
 use rspc::{Config, Router, RouterBuilder};
 use std::{path::PathBuf, sync::Arc};
+
+use self::crud::new_algo_piece;
 
 pub struct Ctx {
     pub client: Arc<prisma::PrismaClient>,
@@ -14,8 +17,16 @@ pub(crate) fn new() -> RouterBuilder<Ctx> {
         .query("version", |t| t(|_, _: ()| env!("CARGO_PKG_VERSION")))
         // .query("algorithms", |t| {
         //     t(|ctx, _: ()| async move {
-        //         let algorithms = ctx.client.algorithm().find_many(vec![]).exec().await?;
+        //         let algorithms = ctx.client.en_algorithm().find_many(vec![]).exec().await?;
         //         Ok(algorithms)
         //     })
         // })
+        //
+        // FIX: error with specta (Type not yet derived for AlgoPiece)
+        .query("new_algo_piece", |t| {
+            t(|ctx, data: AlgoPiece| async move {
+                let created = new_algo_piece(&ctx.client, data).await?;
+                Ok(created)
+            })
+        })
 }
